81c55332b0c2c81a41c4d06805161af37b31d960
Howard Huang <howardhuang96@gmail.com>
2016-05-07 22:02:12 -0700

    Fixes

--- webscraper.py
+++ webscraper.py
@@ -2,23 +2,37 @@
 
 from bs4 import BeautifulSoup
 import requests
+import json
 
 '''To run it yourself:
     1. pip install requests
     2. pip install beautifulsoup4
 '''
 
+"""special characters like: ”  """
+def removeSpecialCharacters(string):
+    string = string.replace("”", "\"")
+    string = string.replace("“", "\"")
+    string = string.replace("’", "\'")
+    string = string.replace("–", "\'")
+    return string
+
 try:
     r = requests.get("http://dailybruin.com/2016/05/06/bruins-united-sails-past-waves-of-change-in-2016-usac-election/")
     soup = BeautifulSoup(r.content, "html.parser")
 
-    headline = soup.find("div", class_="db-post-headline").h1.contents[0]
+    headline = soup.find("div", class_="db-post-headline")
     authors = soup.find("div", class_="db-byline").find_all("a")
+    tempList = []
+    for author in authors:
+        tempList.append(str(author))
+    story = soup.find("div", class_="db-post-content").text
+
+    obj = {u"headline": str(headline), u"authors": tempList}
+
+    print(json.dumps(obj, indent=4))
     with open("output.txt", "w") as text_file:
-        text_file.write(headline)
-        text_file.write("\n")
-        for author in authors:
-            text_file.write(author.contents[0])
-            text_file.write("\n")
+        text_file.write("working")
+        print("GOOD!")
 except requests.exceptions.RequestException as e:    # This is the correct syntax
     print(e)
