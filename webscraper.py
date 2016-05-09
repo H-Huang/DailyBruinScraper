1a732c7a713bb6dd1bd134ed15dca9693efff2e6
Howard Huang <howardhuang96@gmail.com>
2016-05-08 17:09:59 -0700

    multiple images

--- webscraper.py
+++ webscraper.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 '''To run it yourself:
     1. pip install requests
@@ -17,30 +18,49 @@ def removeSpecialCharacters(string):
     string = string.replace("–", "\'")
     return string
 
-try:
-    r = requests.get("http://dailybruin.com/2016/05/06/students-approve-all-four-referenda-on-usac-election-ballot/")
-    soup = BeautifulSoup(r.content, "html.parser")
+"""Enter URLs as strings"""
+urls = [
+    "http://dailybruin.com/2016/05/06/students-approve-all-four-referenda-on-usac-election-ballot/",
+    "http://dailybruin.com/2016/05/06/volunteer-center-explains-rationale-for-ending-collaboration-with-womp/",
+    "http://dailybruin.com/2016/05/05/students-love-of-baking-blooms-into-sourdough-bread-business/"
+]
 
-    headline = str(soup.find("div", class_="db-post-headline").text)
-    authors = soup.find("div", class_="db-byline").find_all("a")
-    tempList = []
-    for author in authors:
-        tempList.append(author.text)
-    story = soup.find("div", class_="db-post-content")
-    story = str(story.text).split("\n<!-- Simple Share Buttons Adder")[0]
-    dates = soup.find_all("h5")
-    postingDate = ""
-    for date in dates:
-        postingDate += str(date.text)
-    imageLink = str(soup.find("div", class_="db-image text-center")).split("src=")[1].split("width=")[0].split("-")[0]
-    image = "<img src=" + imageLink + ".jpg\">"
-    imageCaption = str(soup.find("p", class_="db-image-caption").text).replace("\t", "").replace("\n", "")
+obj = []
+count = 1
 
-    obj = {u"headline": headline, u"postDate": postingDate, u"authors": tempList, u"image": image, u"caption": imageCaption, u"content": story}
+for url in urls:
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
 
-    print(json.dumps(obj, indent=4))
-    with open("output.txt", "w") as text_file:
-        text_file.write(json.dumps(obj, indent=4))
-        print("GOOD!")
-except requests.exceptions.RequestException as e:    # This is the correct syntax
-    print(e)
+        headline = str(soup.find("div", class_="db-post-headline").text)
+        authors = soup.find("div", class_="db-byline").find_all("a")
+        tempList = []
+        for author in authors:
+            tempList.append(author.text)
+        story = soup.find("div", class_="db-post-content")
+        story = str(story.text).split("\n<!-- Simple Share Buttons Adder")[0]
+        dates = soup.find_all("h5")
+        postingDate = ""
+        for date in dates:
+            postingDate += str(date.text)
+        regex = re.compile('.*wp.*')
+        imageList = []
+        images = soup.find_all("img", {"class": regex})
+
+        for image in images:
+            imageLink = str(image).split("src=")[1].split("-")[0]
+            imageList.append("<img src=" + imageLink + ".jpg\">")
+
+        """this needs works"""
+        imageCaption = str(soup.find("p", class_="db-image-caption").text).replace("\t", "").replace("\n", "")
+
+        obj.append({u"headline": headline, u"postDate": postingDate, u"authors": tempList, u"image": imageList, u"caption": imageCaption, u"content": story})
+
+#       print(json.dumps(obj, indent=4))
+        with open("output.txt", "w") as text_file:
+            text_file.write(json.dumps(obj, indent=4))
+            print("URL number " + str(count) + " scraped" + " (" + str(url) + ")")
+            count += 1
+    except requests.exceptions.RequestException as e:    # This is the correct syntax
+        print(e)
